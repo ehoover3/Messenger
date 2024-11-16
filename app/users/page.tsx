712e003c6faf4c18d9c5fde81540c7348ce01c9e
Eric Hoover <ehoover3@yahoo.com>
2024-11-16 15:33:49 -0500

    fix sign out bug

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import { signOut } from "next-auth/react";
 
 const Users = () => {
-  return <button onClick={() => signOut}>Logout</button>;
+  return <button onClick={() => signOut()}>Logout</button>;
 };
 
 export default Users;

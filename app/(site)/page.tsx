f34ea93eb8ea41ac4c983bcd716afb1bc9728699
Eric Hoover <ehoover3@yahoo.com>
2024-11-16 10:03:59 -0500

    set up AuthForm component placehodler

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <Image alt='Logo' height='48' width='48' className='mx-auto w-auto' src='/images/logo.png' />
         <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>Sign in to your account</h2>
       </div>
+      <AuthForm />
     </div>
   );
 }

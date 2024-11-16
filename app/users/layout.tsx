0dfaf93a8162ce554afcd6076f7b7d214811e121
Eric Hoover <ehoover3@yahoo.com>
2024-11-16 18:48:29 -0500

    update users layout

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,3 +1,7 @@
 export default async function UsersLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return;
+
+  <Sidebar>
+    <div className='h-full'>{children}</div>
+  </Sidebar>;
 }

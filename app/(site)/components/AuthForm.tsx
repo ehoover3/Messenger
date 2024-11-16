426f6ca87958d208217b188e961f6531b0789f45
Eric Hoover <ehoover3@yahoo.com>
2024-11-16 10:11:59 -0500

    initialize react form

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,34 @@
 "use client";
 
+import { useCallback, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
 const AuthForm = () => {
+  const [variant, setVariant] = useState("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
   return <div>Auth Form</div>;
 };
 

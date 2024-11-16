d880702c1e00362fb5b02708b9d05b41af057d70
Eric Hoover <ehoover3@yahoo.com>
2024-11-16 18:57:18 -0500

    create desktopsidebar component

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,7 +1,9 @@
-export default async function UsersLayout({ children }: { children: React.ReactNode }) {
-  return;
+import Sidebar from "../components/sidebar/Sidebar";
 
-  <Sidebar>
-    <div className='h-full'>{children}</div>
-  </Sidebar>;
+export default async function UsersLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Sidebar>
+      <div className='h-full'>{children}</div>
+    </Sidebar>
+  );
 }

d206653940e44847e3ff5ec5899000ce2b0d7949
Eric Hoover <ehoover3@yahoo.com>
2024-11-16 11:03:58 -0500

    update sign up / login text and buttons

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,7 +2,7 @@
 
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { BsGithub } from "react-icons/bs";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 import Input from "@/app/components/inputs/Input";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
@@ -74,7 +74,14 @@ const AuthForm = () => {
             </div>
           </div>
           <div className='mt-6 flex gap-2'>
-            <AuthSocialButton icon={BsGithub} />
+            <AuthSocialButton icon={BsGithub} onClick={() => socialAction("github")} />
+            <AuthSocialButton icon={BsGoogle} onClick={() => socialAction("google")} />
+          </div>
+        </div>
+        <div className='flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500'>
+          <div>{variant === "LOGIN" ? "New to messenger?" : "Already have an account"}</div>
+          <div onClick={toggleVariant} className='underline cursor-pointer'>
+            {variant === "LOGIN" ? "Create an account" : "Login "}
           </div>
         </div>
       </div>

9a1b09feea60e3d78eff91743f4a7a05fd48f720
Eric Hoover <ehoover3@yahoo.com>
2024-11-16 10:23:57 -0500

    create input form component

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
@@ -40,7 +41,21 @@ const AuthForm = () => {
     }
   };
 
-  return <div>Auth Form</div>;
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth Social Sign In
+  };
+
+  return (
+    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
+          <Input label='Email' />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;

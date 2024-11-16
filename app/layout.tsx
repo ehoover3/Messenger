1cf68cdb292308d5b03eedfd01c4f6224c99b7b0
Eric Hoover <ehoover3@yahoo.com>
2024-11-16 15:31:23 -0500

    create user signout button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,8 +14,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   );
---
+++ app/users/page.tsx
@@ -0,0 +1,7 @@
+import { signOut } from "next-auth/react";
+
+const Users = () => {
+  return <button onClick={() => signOut}>Logout</button>;
+};
+
+export default Users;

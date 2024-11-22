c3a7a638af117d621bc85a754a9da0ab850a7f4d
Eric Hoover <ehoover3@yahoo.com>
2024-11-22 07:18:05 -0500

    fix desktop item styling

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import clsx from "clsx";
 import Link from "next/link";
 
@@ -11,7 +9,7 @@ interface DesktopItemProps {
   active?: boolean;
 }
 
-const DesktopItem: React.FC<DesktopItemProps> = ({ label, icon: Icon, href, onClick, active }) => {
+export default function DesktopItem({ label, icon: Icon, href, onClick, active }: DesktopItemProps) {
   const handleClick = () => {
     if (onClick) {
       return onClick();
@@ -20,12 +18,10 @@ const DesktopItem: React.FC<DesktopItemProps> = ({ label, icon: Icon, href, onCl
 
   return (
     <li onClick={handleClick}>
-      <Link href={href} className={clsx(`group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100`, active && "bg-gray-100 text-black")}>
-        <Icon className='h-6 w-6 shrink-0' />
-        <span>{label}</span>
+      <Link className={clsx(`group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100`, active && "bg-gray-100 text-black")} href={href}>
+        <Icon className='w-6 h-6 shrink-0' />
+        <span className='sr-only'>{label}</span>
       </Link>
     </li>
   );
-};
-
-export default DesktopItem;
+}

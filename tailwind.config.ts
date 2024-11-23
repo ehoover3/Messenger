ec7f123d4e544ef6202fd401da10902fc3bbe7ec
Eric Hoover <ehoover3@yahoo.com>
2024-11-23 11:18:20 -0500

    remove certificates

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")({ strategy: "class" })],
-} satisfies Config;

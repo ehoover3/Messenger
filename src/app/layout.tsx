706d0e57b79568e802bbdf02aa496ed72d32b735
Eric Hoover <ehoover3@yahoo.com>
2024-11-23 11:10:20 -0500

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import { Inter } from "next/font/google";
-import ToasterContext from "./context/ToasterContext";
+import ActiveStatus from "./components/ActiveStatus";
+import { Providers } from "./components/theme/Providers";
 import AuthContext from "./context/AuthContext";
+import ToasterContext from "./context/ToasterContext";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
-  title: "Eric Hoover - Messenger",
-  description: "Messenger App",
+  title: "NextJs Messenger Clone",
+  description: "NextJs Messenger Clone",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body suppressHydrationWarning={true}>
         <AuthContext>
-          <ToasterContext />
-          {children}
+          <Providers>
+            <>
+              <ActiveStatus />
+              <ToasterContext />
+              {children}
+            </>
+          </Providers>
         </AuthContext>
       </body>
     </html>
